5da390218be603cacc1cf9640abe226e31968d72
Manoj Kumar <31114842+MK-Logs@users.noreply.github.com>
2022-04-17 03:34:42 -0500

    Update StreamHandlerTemplate.scala

--- StreamHandlerTemplate.scala
+++ StreamHandlerTemplate.scala
@@ -11,8 +11,7 @@ case class DeviceData(device: String, temp: Double, humd: Double, pres: Double)
 
 object StreamHandler {
 	def main(args: Array[String]) {
-
-		// Initialization
+		
 		val spark = SparkSession
 			.builder
 			.appName("Stream Handler")
@@ -21,7 +20,6 @@ object StreamHandler {
 
 		import spark.implicits._
 
-		// ReadStream
 		val inputDF = spark
 			.readStream
 			.format("kafka") // org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.5
@@ -29,10 +27,8 @@ object StreamHandler {
 			.option("subscribe", "weather")
 			.load()
 
-		// Byte String Conversion
 		val rawDF = inputDF.selectExpr("CAST(value AS STRING)").as[String]
 
-		// Splitting
 		val expandedDF = rawDF.map(row => row.split(","))
 			.map(row => DeviceData(
 				row(1),
@@ -41,22 +37,17 @@ object StreamHandler {
 				row(4).toDouble
 			))
 
-		// Aggregation
 		val summaryDf = expandedDF
 			.groupBy("device")
 			.agg(avg("temp"), avg("humd"), avg("pres"))
 
-		// Creating UUIDs
 		val makeUUID = udf(() => Uuids.timeBased().toString)
 
-		// Adding UUIDs
-		// Casandra Table Schema
 		val summaryWithIDs = summaryDf.withColumn("uuid", makeUUID())
 			.withColumnRenamed("avg(temp)", "temp")
 			.withColumnRenamed("avg(humd)", "humd")
 			.withColumnRenamed("avg(pres)", "pres")
 
-		// Writing DataFrame
 		val query = summaryWithIDs
 			.writeStream
 			.trigger(Trigger.ProcessingTime("5 seconds"))
